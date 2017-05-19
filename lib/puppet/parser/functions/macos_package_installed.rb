80f8ccde0bdb0005a6de51ff24b7d3ad6d3a1faf
Graham Gilbert <graham@grahamgilbert.com>
2017-05-19 13:34:52 -0700

    Rubocop tests

--- lib/puppet/parser/functions/macos_package_installed.rb
+++ lib/puppet/parser/functions/macos_package_installed.rb
@@ -3,16 +3,20 @@
 #
 require 'puppet/util/package'
 
+# Returns true if the specified or newer version of the package is installed
 module Puppet::Parser::Functions
   newfunction(:macos_package_installed, type: :rvalue, doc: <<-EOS
 Returns true if the specified or newer version of the package is installed.
     EOS
              ) do |args|
 
-    raise(Puppet::ParseError, 'macos_package_installed(): ' \
-    "Wrong number of arguments given (#{args.size} for 2)") if args.size != 2
+    if args.size != 2
+      raise(Puppet::ParseError, 'macos_package_installed(): ' \
+      "Wrong number of arguments given (#{args.size} for 2)")
+    end
 
-    raise(Puppet::ParseError, 'This can only be used on macOS') unless lookupvar('operatingsystem').casecmp('darwin').zero?
+    raise(Puppet::ParseError, 'This can only be used on macOS') unless \
+     lookupvar('operatingsystem').casecmp('darwin').zero?
 
     # Alright, let's see what version is installed (if any)
     installed_packages = lookupvar('installed_packages')
@@ -21,7 +25,10 @@ Returns true if the specified or newer version of the package is installed.
 
     # the receipt is here, check the version against what we want
     if found == true
-      version_check = Puppet::Util::Package.versioncmp(installed_packages[args[0]]['version'], args[1])
+      version_check = Puppet::Util::Package.versioncmp(
+        installed_packages[args[0]]['version'],
+        args[1]
+      )
       return true if version_check != -1
     end
 
