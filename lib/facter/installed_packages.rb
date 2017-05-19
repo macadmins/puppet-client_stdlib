a743da66e11719fa9a6ac99a7b7b21f7dcd1528f
Brett Demetris <brett.demetris@airbnb.com>
2017-05-19 09:47:01 -0700

    now does windows package installs

--- lib/facter/installed_packages.rb
+++ lib/facter/installed_packages.rb
@@ -21,3 +21,47 @@ Facter.add('installed_packages') do
     items
   end
 end
+
+# yes, windows machines exist
+# set to break if powershell cannot be found at the defined path
+Facter.add('installed_packages') do
+  confine osfamily: 'Windows'
+
+  setcode do
+
+  require 'json'
+
+	powershell = 'C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+	command = 'gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Convertto-json'
+
+ 	if File.exists?(powershell)
+
+	raw = Facter::Util::Resolution.exec(%Q{#{powershell} -command "#{command}"})
+
+	installed_packages = {}
+
+	items = JSON.parse(raw)
+
+	for item in items[1..-1]
+
+		if item['InstallLocation'].nil? || item['InstallLocation'] == ""
+			volume = ""
+		else
+			volume = item['InstallLocation'][0..2]
+		end
+
+		if item['DisplayName'].nil?
+			item['DisplayName'] == ""
+		end
+
+		installed_packages[item['DisplayName']] = {
+			'version' => item['DisplayVersion'],
+			'installdate' => item['InstallDate'],
+			'installlocation' => item['InstallLocation'],
+			'volume' => volume
+		}
+	end
+	installed_packages
+  end
+ end
+end
