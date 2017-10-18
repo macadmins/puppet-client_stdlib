1031d207349735111294535190032b50ed51baf1
Graham Gilbert <graham@grahamgilbert.com>
2017-10-18 11:01:00 -0700

    Linter

--- lib/facter/depnotify_running.rb
+++ lib/facter/depnotify_running.rb
@@ -2,10 +2,8 @@ Facter.add('depnotify_running') do
   confine osfamily: 'Darwin'
   setcode do
     depnotify_running = false
-    output = Facter::Util::Resolution.exec("/bin/ps axo pid,command | grep \"[D]EPNotify.app\"")
-    if $CHILD_STATUS.exitstatus === 0
-      depnotify_running = true
-    end
+    _ = Facter::Util::Resolution.exec('/bin/ps axo pid,command | grep "[D]EPNotify.app"')
+    depnotify_running = true if $CHILD_STATUS.exitstatus.zero?
     depnotify_running
   end
 end
