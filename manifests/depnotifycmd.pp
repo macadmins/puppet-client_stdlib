8ac027885257605149bbc303ca37f0c0f3f489eb
Graham Gilbert <graham@grahamgilbert.com>
2017-10-19 10:19:05 -0700

    Support duplicate messages

--- manifests/depnotifycmd.pp
+++ manifests/depnotifycmd.pp
@@ -1,11 +1,18 @@
 # == Class: client_stdlib::depnotifycmd
 # Uses file_line to add the line to the end of the log
 
-define client_stdlib::depnotifycmd (){
+define client_stdlib::depnotifycmd (
+  String $message = '',
+  ){
+    if $message = '' {
+      $output = $title
+    } else {
+      $output = $message
+    }
   if $facts['depnotify_running'] == true {
     file_line {"DEPNotifyCmd ${name}":
       path => '/var/tmp/depnotify.log',
-      line => $name
+      line => $output
     }
   }
 }
