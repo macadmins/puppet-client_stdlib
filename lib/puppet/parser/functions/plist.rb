a9727d19ba66694998146b69632dc4e26aec09b2
Graham Gilbert <graham@grahamgilbert.com>
2021-06-03 23:14:07 -0700

    fmt

--- lib/puppet/parser/functions/plist.rb
+++ lib/puppet/parser/functions/plist.rb
@@ -1,6 +1,7 @@
 # ruby hash
 
 require 'cfpropertylist' if Puppet.features.cfpropertylist?
+require 'puppet/util/plist' if Puppet.features.cfpropertylist?
 
 # Accepts a hash as input and returns a plist
 module Puppet::Parser::Functions
@@ -9,6 +10,6 @@ module Puppet::Parser::Functions
     format = args[1]      || :xml
     plist_to_save       = CFPropertyList::List.new
     plist_to_save.value = CFPropertyList.guess(hash, :convert_unknown_to_string => true)
-    return plist_to_save.to_str(to_format(format), :formatted => true)
+    return plist_to_save.to_str(Puppet::Util::Plist.to_format(format), :formatted => true)
   end
 end
