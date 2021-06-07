11ea57311a75b6b84169e6d82b1114aff723e4e4
Graham Gilbert <graham@grahamgilbert.com>
2021-06-07 14:21:32 -0700

    lin

--- manifests/depnotifycmd.pp
+++ manifests/depnotifycmd.pp
@@ -1,14 +1,14 @@
-# == Class: client_stdlib::depnotifycmd
+# == Define: client_stdlib::depnotifycmd
 # Uses file_line to add the line to the end of the log
 
 define client_stdlib::depnotifycmd (
   String $message = '',
-  ){
-    if $message == '' {
-      $output = $title
-    } else {
-      $output = $message
-    }
+){
+  if $message == '' {
+    $output = $title
+  } else {
+    $output = $message
+  }
   if $facts['depnotify_running'] == true {
     exec { "DEPNotifyCmd ${name}":
       command => "/bin/echo \"${output}\" >> /var/tmp/depnotify.log",
