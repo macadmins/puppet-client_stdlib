169657e3b9adfb07ea03f6aba94d66600213b260
Graham Gilbert <graham@grahamgilbert.com>
2021-06-06 20:48:58 -0700

    Try to appease linter gods

--- lib/puppet/parser/functions/plist.rb
+++ lib/puppet/parser/functions/plist.rb
@@ -9,7 +9,9 @@ module Puppet::Parser::Functions
     hash   = args[0]      || {}
     format = args[1]      || :xml
     plist_to_save       = CFPropertyList::List.new
-    plist_to_save.value = CFPropertyList.guess(hash, {:convert_unknown_to_string => true})
-    return plist_to_save.to_str(Puppet::Util::Plist.to_format(format), {:formatted => true})
+    opts = {:convert_unknown_to_string => true}
+    plist_to_save.value = CFPropertyList.guess(hash, opts)
+    fmt_opts = {:formatted => true}
+    return plist_to_save.to_str(Puppet::Util::Plist.to_format(format), fmt_opts)
   end
 end
